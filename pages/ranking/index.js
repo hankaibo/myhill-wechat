5070852da277ce3cbf9a0f56bd8c80b87535730a
hankaibo <hankaibo@hotmail.com>
2020-05-11 01:16:41 +0800

    feat: 排行榜页面开发

---
+++ pages/ranking/index.js
@@ -0,0 +1,338 @@
+// pages/ranking/index.js
+//获取应用实例
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userList: [{
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+      {
+        openid: 1,
+        avatarUrl: '',
+        nickName: '张三',
+        score: 100,
+        like: 1
+      },
+    ],
+    userInfo: {},
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    //调用应用实例的方法获取全局数据
+    this.setData({
+      userInfo: app.globalData.userInfo
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
