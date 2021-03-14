8dfb6d153a6dd01b90a726c1f2861ae67dde66ab
hankaibo <hankaibo@hotmail.com>
2021-03-15 01:00:29 +0800

    chore: 更新request.js

--- utils/request.js
+++ utils/request.js
@@ -1,9 +1,3 @@
-const userInfo = wx.getStorageSync('userInfo');
-let token = null;
-if (userInfo) {
-  token = JSON.parse(userInfo).token;
-}
-
 const request = (url, method = 'GET', data) => {
   let promise = new Promise((resolve, reject) => {
     wx.request({
@@ -11,7 +5,7 @@ const request = (url, method = 'GET', data) => {
       method,
       data,
       header: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${wx.getStorageSync('miniToken')}`,
       },
       success: function (res) {
         resolve(res);
